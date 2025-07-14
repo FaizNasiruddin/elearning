602531f37ba445842c38dfecf9dcb07c172f80ae
FaizNasiruddin <faiznasiruddin8@gmail.com>
2025-07-14 23:18:20 +0800

    commit

--- resources/views/admin-subject-particular.blade.php
+++ resources/views/admin-subject-particular.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Subject Details</title>
-    <link rel="stylesheet" href="{{ asset('css/admin-subject-student.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/all.css') }}">
 </head>
 <body>
 
